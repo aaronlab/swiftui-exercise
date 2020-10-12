e53d58893b2285c099a052ecd9ef9021d54f506f
Aaron Lee <aaronlee@Aarons-MacBook-Pro.local>
2020-10-12 22:31:50 +0900

    Adding Images to the List

--- Hiking/Hiking/ContentView.swift
+++ Hiking/Hiking/ContentView.swift
@@ -8,21 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+
     let hikes = Hike.all()
-    
+
     var body: some View {
-        
+
         List(self.hikes, id: \.name) { hike in
-            Text(hike.name)
-                .font(.title)
+            HikeCell(hike: hike)
         }
-        
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct HikeCell: View {
+
+    let hike: Hike
+
+    var body: some View {
+        HStack {
+            Image(hike.imageURL)
+                .resizable()
+                .frame(width: 100, height: 100)
+                .cornerRadius(16)
+
+            VStack(alignment: .leading) {
+                Text(hike.name)
+                Text("\(String(format: "%.2f", hike.miles)) miles")
+            }
+        }
     }
 }
