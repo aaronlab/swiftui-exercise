4e402c968621cb9ef9ba64ac4d04e27f2da2bbd5
Aaron Lee <aaronlee@Aarons-MacBook-Pro.local>
2020-10-12 22:54:56 +0900

    Tap & Zooming Animation

--- Hiking/Hiking/HikingDetail.swift
+++ Hiking/Hiking/HikingDetail.swift
@@ -10,22 +10,29 @@ import SwiftUI
 struct HikingDetail: View {
 
     let hike: Hike
+    @State private var zoomed: Bool = false
 
     var body: some View {
         VStack {
             Image(hike.imageURL)
                 .resizable()
-                .aspectRatio(contentMode: .fit)
+                .aspectRatio(contentMode: self.zoomed ? .fill : .fit)
+                .onTapGesture {
+                    withAnimation {
+                        self.zoomed.toggle()
+                    }
+            }
 
             Text(hike.name)
                 .font(.title)
             Text("\(String(format: "%2.f", hike.miles)) miles")
-        }
+        }.navigationTitle(hike.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct HikingDetail_Previews: PreviewProvider {
     static var previews: some View {
-        HikingDetail(hike: Hike(name: "Hiking2", imageURL: "something2", miles: 6.8))
+        HikingDetail(hike: Hike(name: "Toronto", imageURL: "toronto", miles: 6))
     }
 }
