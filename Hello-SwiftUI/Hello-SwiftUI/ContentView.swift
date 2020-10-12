0d0e3025895be562ed69169b7e8f6597e86ac62b
Aaron Lee <aaronlee@Aarons-MacBook-Pro.local>
2020-10-12 22:10:43 +0900

    Image

--- Hello-SwiftUI/Hello-SwiftUI/ContentView.swift
+++ Hello-SwiftUI/Hello-SwiftUI/ContentView.swift
@@ -10,23 +10,32 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Image("sampleImage")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(20.0)
+                .padding(.all)
+                .clipShape(Circle())
+            
             Text("First Line")
                 .font(.largeTitle)
                 .foregroundColor(.green)
             Text("Second Line")
                 .font(.title)
+            
             HStack {
                 Text("First Line")
                 Spacer()
                 Text("Second Line")
             }
+            
             ZStack {
                 Text("First Line")
                     .foregroundColor(.orange)
                 Text("Second Line")
                     .padding(.all)
             }
-        }.padding()
+        }
     }
 }
 
