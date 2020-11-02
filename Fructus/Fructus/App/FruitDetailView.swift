58d186838375d6d6a17c4473be9383312beb6d26
Aaron Lee <aaronlab.net@gmail.com>
2020-11-02 22:36:22 +0900

    GroupBox+DisclosureGroup

--- Fructus/Fructus/App/FruitDetailView.swift
+++ Fructus/Fructus/App/FruitDetailView.swift
@@ -34,6 +34,7 @@ struct FruitDetailView: View {
                             .multilineTextAlignment(.leading)
                         
                         // NUTRIENTS
+                        FruitNutrientsView(fruit: fruit)
                         
                         // SUBHEADLINE
                         Text("Learn more about \(fruit.title.uppercased())")
