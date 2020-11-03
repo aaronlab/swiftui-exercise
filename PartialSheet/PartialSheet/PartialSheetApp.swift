97efb572d19595fa154725595728bb66ab85ec67
Aaron Lee <aaronlab.net@gmail.com>
2020-11-03 20:41:39 +0900

    Initial Commit

---
+++ PartialSheet/PartialSheet/PartialSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartialSheetApp.swift
+//  PartialSheet
+//
+//  Created by Aaron Lee on 2020-11-03.
+//
+
+import SwiftUI
+
+@main
+struct PartialSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
