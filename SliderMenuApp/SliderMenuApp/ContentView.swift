403eed56f8a21bbd3716b79bfca8890fe3a3a8a3
Aaron Lee <aaronlab.net@gmail.com>
2020-11-06 22:37:50 +0900

    Init SliderMenuApp

---
+++ SliderMenuApp/SliderMenuApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SliderMenuApp
+//
+//  Created by Aaron Lee on 2020-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SliderMenuApp/SliderMenuApp/SliderMenuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderMenuAppApp.swift
+//  SliderMenuApp
+//
+//  Created by Aaron Lee on 2020-11-06.
+//
+
+import SwiftUI
+
+@main
+struct SliderMenuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
