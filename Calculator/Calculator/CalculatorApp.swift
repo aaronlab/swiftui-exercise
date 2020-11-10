db37db6f2a49296bd25665a3710d1fd57cdf90ee
Aaron Lee <aaronlab.net@gmail.com>
2020-11-10 21:56:07 +0900

    feat: Add Calculator project

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Aaron Lee on 2020-11-10.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
