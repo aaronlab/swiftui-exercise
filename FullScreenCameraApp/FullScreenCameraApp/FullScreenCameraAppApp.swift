0cd0a8f2734bb77e343998fafc502f30454a2c77
Aaron Lee <aaronlab.net@gmail.com>
2020-11-12 20:42:02 +0900

    FullScreenCameraApp Init

---
+++ FullScreenCameraApp/FullScreenCameraApp/FullScreenCameraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FullScreenCameraAppApp.swift
+//  FullScreenCameraApp
+//
+//  Created by Aaron Lee on 2020-11-12.
+//
+
+import SwiftUI
+
+@main
+struct FullScreenCameraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
