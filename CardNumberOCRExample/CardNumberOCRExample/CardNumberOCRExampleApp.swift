1820a9aaca482658deb39a2b4c8ecb6f93cadda9
Aaron Lee <aaronlab.net@gmail.com>
2020-11-13 20:33:40 +0900

    CardNumberOCRExample

---
+++ CardNumberOCRExample/CardNumberOCRExample/CardNumberOCRExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardNumberOCRExampleApp.swift
+//  CardNumberOCRExample
+//
+//  Created by Aaron Lee on 2020-11-13.
+//
+
+import SwiftUI
+
+@main
+struct CardNumberOCRExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardNumberOCRExample/CardNumberOCRExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CardNumberOCRExample
+//
+//  Created by Aaron Lee on 2020-11-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
