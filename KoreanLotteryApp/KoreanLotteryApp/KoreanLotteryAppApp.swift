1fc7270445b9032c45dfe8789fd102c401b1dd94
Aaron Lee <aaronlab.net@gmail.com>
2020-11-15 22:03:25 +0900

    Korean Lottery App Init

---
+++ KoreanLotteryApp/KoreanLotteryApp/KoreanLotteryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoreanLotteryAppApp.swift
+//  KoreanLotteryApp
+//
+//  Created by Aaron Lee on 2020-11-15.
+//
+
+import SwiftUI
+
+@main
+struct KoreanLotteryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ KoreanLotteryApp/KoreanLotteryApp/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  KoreanLotteryApp
+//
+//  Created by Aaron Lee on 2020-11-15.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
