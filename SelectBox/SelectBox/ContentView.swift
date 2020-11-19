66a5b0021762fb60a44a72fb1014bca30cdc7e58
Aaron Lee <aaronlab.net@gmail.com>
2020-11-19 22:47:35 +0900

    View

--- SelectBox/SelectBox/ContentView.swift
+++ SelectBox/SelectBox/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject private var viewModel: ViewModel = ViewModel()
+    @State private var alertIsShowing: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+        
+        
+        ZStack(alignment: .center) {
+            ScrollView {
+                
+                VStack {
+                    
+                    if let options = viewModel.options {
+                        
+                        ForEach(options, id: \.id) { option in
+                            BoxCell(isSelected: option.title == viewModel.selectedOption?.title, title: option.title)
+                                .padding(16)
+                                .onTapGesture {
+                                    withAnimation {
+                                        self.alertIsShowing.toggle()
+                                    }
+                                    
+                                    withAnimation(.easeInOut(duration: 0.3)) {
+                                        viewModel.selectedOption = option
+                                    }
+                                }
+                            Divider()
+                        }
+                        
+                    }
+                    
+                } //: VSTACK
+            } //: SCROLL
+            
+            if alertIsShowing {
+                AlertView(isPresented: $alertIsShowing, option: $viewModel.selectedOption)
+            }
+        } //: ZSTACK
+        .onAppear {
+            viewModel.loadOptions()
+        }
+        
     }
 }
