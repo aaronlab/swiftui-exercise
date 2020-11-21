f96b2e57b34a912e5161fd3e6cddeb9eae9be07f
Aaron Lee <aaronlab.net@gmail.com>
2020-11-21 21:52:10 +0900

    Init My Reminder

---
+++ My Reminder/My Reminder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  My Reminder
+//
+//  Created by Aaron Lee on 2020-11-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
