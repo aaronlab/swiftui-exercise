211b908ca4ce62be8266f21523833a5b3cbc8454
Aaron Lee <aaronlab.net@gmail.com>
2020-11-21 23:53:50 +0900

    Reminder List View

--- My Reminder/My Reminder/ReminderList.swift
+++ My Reminder/My Reminder/ReminderList.swift
@@ -8,14 +8,92 @@
 import SwiftUI
 
 struct ReminderList: View {
+    // MARK: - PROPERTIES
+    
+    @ObservedObject private var reminderListVM = ReminderListViewModel()
+    @State private var addNewPresented = false
+    
+    // MARK: - INIT
+    
+    init() {
+        UITableView.appearance().tableFooterView = UIView()
+        UITableView.appearance().separatorStyle = .none
+        UITableView.appearance().backgroundColor = .clear
+        UITableViewCell.appearance().selectionStyle = .none
+    }
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            VStack {
+                
+                ZStack(alignment: .bottomTrailing) {
+                    
+                    ReminderHeader().padding(.top, 16)
+                    Button(action: { self.addNewPresented.toggle() }) {
+                        Image(systemName: "plus")
+                            .font(.title)
+                            .foregroundColor(.white)
+                    }.padding([.trailing, .bottom], 20)
+                    .sheet(isPresented: $addNewPresented) {
+                        // ADD NEW
+                        Text("ADD")
+                    }
+                } //: ZSTACK
+                List {
+                    ForEach(self.reminderListVM.reminders.indices, id: \.self) { index in
+                        ZStack {
+                            
+                            ReminderCell(reminderVM: self.reminderListVM.reminders[index])
+                                .defaultShadow(radius: 10)
+                            
+                            NavigationLink(destination:
+                                            Text("Update")
+                                            .navigationTitle("")
+                                            .navigationBarHidden(true)
+                            ) {
+                                EmptyView()
+                            } //: NAVIGATION LINK
+                            .buttonStyle(PlainButtonStyle())
+                        } //: ZSTACK
+                    } //: FOR EACH
+                    .onDelete(perform: delete(at:))
+                } //: LIST
+                .onAppear {
+                    self.reload()
+                }
+            } //: VSTACK
+            .background(Color.gray.edgesIgnoringSafeArea(.all))
+            .navigationBarHidden(true)
+            .navigationBarTitle("", displayMode: .inline)
+        } //: NAVIGATION
+    }
+    
+    private func delete(at offsets: IndexSet) {
+        for index in offsets {
+            self.reminderListVM.removeReminder(at: index)
+        }
+        self.reload()
+    }
+    
+    private func reload() {
+        self.reminderListVM.fetchReminders()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ReminderList()
+struct ReminderHeader: View {
+    
+    var body: some View {
+        VStack {
+            Text("Reminder")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity, maxHeight: 60)
+                .background(Rectangle().fill(Color.clear))
+        } //: VSTACK
+        .edgesIgnoringSafeArea(.all)
     }
+    
 }
