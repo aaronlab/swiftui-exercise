ed2037e64be02b1108389c6d575a3c2df0eef0e1
Aaron Lee <aaronlab.net@gmail.com>
2020-11-25 19:06:19 +0900

    Init CustomTextField

---
+++ CustomTextField/CustomTextField/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomTextField
+//
+//  Created by Aaron Lee on 2020-11-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomTextField/CustomTextField/CustomTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTextFieldApp.swift
+//  CustomTextField
+//
+//  Created by Aaron Lee on 2020-11-25.
+//
+
+import SwiftUI
+
+@main
+struct CustomTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
