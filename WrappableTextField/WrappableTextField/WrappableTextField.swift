c317fe2184e6a1cdc09d504f5bfdab5741d5cfcd
Aaron Lee <aaronlab.net@gmail.com>
2020-11-27 21:48:49 +0900

    WrappableTextField Delegate Methods

--- WrappableTextField/WrappableTextField/WrappableTextField.swift
+++ WrappableTextField/WrappableTextField/WrappableTextField.swift
@@ -13,15 +13,24 @@ class WrappableTextField: UITextField, UITextFieldDelegate {
     var onCommitted: (() -> Void)?
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
+        if let next = textField.superview?.superview?.viewWithTag(textField.tag + 1) as? UITextField {
+            next.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        return false
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
+        if let value = textField.text as NSString? {
+            let newValue = value.replacingCharacters(in: range, with: string)
+            onEditing?(newValue as String)
+        }
+        return true
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        
+        onCommitted?()
     }
     
 }
