303914be9580dbe48bcaa59e88e2b7d8b654c95a
Aaron Lee <aaronlab.net@gmail.com>
2020-11-28 17:04:25 +0900

    Init DropDownMenu App

---
+++ DropDownMenu/DropDownMenu/DropDownMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropDownMenuApp.swift
+//  DropDownMenu
+//
+//  Created by Aaron Lee on 2020-11-28.
+//
+
+import SwiftUI
+
+@main
+struct DropDownMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
