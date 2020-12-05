e214e05871af98d82261c5ecb01ced2a8a5bfa1c
Aaron Lee <aaronlab.net@gmail.com>
2020-12-05 19:37:31 +0900

    Add ContentView

---
+++ CustomSearchNavBar/CustomSearchNavBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomSearchNavBar
+//
+//  Created by Aaron Lee on 2020-12-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomSearchNavBar/CustomSearchNavBar/CustomSearchNavBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSearchNavBarApp.swift
+//  CustomSearchNavBar
+//
+//  Created by Aaron Lee on 2020-12-05.
+//
+
+import SwiftUI
+
+@main
+struct CustomSearchNavBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
