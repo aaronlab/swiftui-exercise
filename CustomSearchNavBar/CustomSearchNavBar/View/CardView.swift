3d5cfcae8d32d55a18553181abc1fa527278d204
Aaron Lee <aaronlab.net@gmail.com>
2020-12-05 19:47:25 +0900

    Create CardView.swift

---
+++ CustomSearchNavBar/CustomSearchNavBar/View/CardView.swift
@@ -0,0 +1,63 @@
+//
+//  CardView.swift
+//  CustomSearchNavBar
+//
+//  Created by Aaron Lee on 2020-12-05.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    
+    var item: AppItem
+    
+    var body: some View {
+        
+        HStack(spacing: 15) {
+            
+            Image(item.name)
+                .resizable()
+                .frame(width: 65, height: 65)
+            
+            VStack {
+                
+                HStack {
+                    
+                    VStack(alignment: .leading, spacing: 15) {
+                        
+                        Text(item.name)
+                            .font(.title2)
+                        
+                        Text(item.source)
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    
+                    Spacer(minLength: 10)
+                    
+                    VStack {
+                        
+                        Button(action: {}) {
+                            Text("GET")
+                                .fontWeight(.heavy)
+                                .padding(.vertical, 8)
+                                .padding(.horizontal, 20)
+                                .background(Color.primary.opacity(0.08))
+                                .clipShape(Capsule())
+                        }
+                        
+                        Text("In App Purchases")
+                            .font(.caption2)
+                            .foregroundColor(.gray)
+                        
+                    }
+                }
+                
+                Divider()
+                
+            }
+        }
+        .padding(.horizontal)
+        
+    }
+}
