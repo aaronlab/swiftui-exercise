8769c4fcce0f1665754da0eea2d35689b849cdac
Aaron Lee <aaronlab.net@gmail.com>
2020-12-05 20:10:11 +0900

    Update ContentView.swift

--- CustomSearchNavBar/CustomSearchNavBar/ContentView.swift
+++ CustomSearchNavBar/CustomSearchNavBar/ContentView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var filteredItems = AppItem.placeholder()
+    
     var body: some View {
-        Home()
+        CustomNavigationView(view: AnyView(Home(filteredItems: $filteredItems)),
+                             placeHolder: "Apps, Games",
+                             largeTitle: true,
+                             title: "Aaron Lab",
+                             onSearch: { value in
+                                if value != "" {
+                                    self.filteredItems = AppItem.placeholder().filter { $0.name.lowercased().contains(value.lowercased()) }
+                                } else {
+                                    self.filteredItems = AppItem.placeholder()
+                                }
+                             }, onCancel: {
+                                self.filteredItems = AppItem.placeholder()
+                             })
+            .ignoresSafeArea()
     }
 }
 
