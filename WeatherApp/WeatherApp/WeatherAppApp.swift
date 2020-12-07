a6da3175895805d7a54f593795cc0c2ddfbab8d9
Aaron Lee <aaronlab.net@gmail.com>
2020-12-07 22:01:28 +0900

    Init WeatherApp

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Aaron Lee on 2020-12-07.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
