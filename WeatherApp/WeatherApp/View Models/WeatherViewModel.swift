618f7a2f7a03bf38c64bada8195225a586af478b
Aaron Lee <aaronlab.net@gmail.com>
2020-12-07 22:22:18 +0900

    WeatherViewModel

---
+++ WeatherApp/WeatherApp/View Models/WeatherViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  WeatherViewModel.swift
+//  WeatherApp
+//
+//  Created by Aaron Lee on 2020-12-07.
+//
+
+import Foundation
+import Combine
+
+final class WeatherViewModel: ObservableObject {
+    
+    private var weatherService: WeatherService!
+    
+    @Published var weather = Weather()
+    
+    init() {
+        self.weatherService = WeatherService()
+    }
+    
+    var temperature: String {
+        if let temp = self.weather.temp {
+            return String(format: "%.0f", temp)
+        } else {
+            return ""
+        }
+    }
+    
+    var humidity: String {
+        if let humidity = self.weather.humidity {
+            return String(format: "%.0f", humidity)
+        } else {
+            return ""
+        }
+    }
+    
+    var cityName: String = ""
+    
+    func search() {
+        if let city = self.cityName.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) {
+            fetchWeather(by: city)
+        }
+    }
+    
+    private func fetchWeather(by city: String) {
+        
+        self.weatherService.getWeather(city: city) { weather in
+            if let weather = weather {
+                self.weather = weather
+            }
+        }
+        
+    }
+    
+}
