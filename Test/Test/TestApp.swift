ba13bca50b69cf76c4c9f7274d2eef5e769b0512
Aaron Lee <aaronlab.net@gmail.com>
2020-12-08 21:53:54 +0900

    Test App

---
+++ Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Aaron Lee on 2020-12-08.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
