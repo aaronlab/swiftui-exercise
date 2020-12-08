a9b1733206d03f5af03d565309ff139ad1050bfd
Aaron Lee <aaronlab.net@gmail.com>
2020-12-08 21:14:31 +0900

    Init Pinterest

---
+++ Pinterest/Shared/PinterestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinterestApp.swift
+//  Shared
+//
+//  Created by Aaron Lee on 2020-12-08.
+//
+
+import SwiftUI
+
+@main
+struct PinterestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
