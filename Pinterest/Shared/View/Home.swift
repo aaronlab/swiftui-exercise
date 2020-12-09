3a51a4eac604c8bd8560947264692c1fd4dc53bd
Aaron Lee <aaronlab.net@gmail.com>
2020-12-09 21:43:22 +0900

    Search Bar

--- Pinterest/Shared/View/Home.swift
+++ Pinterest/Shared/View/Home.swift
@@ -10,13 +10,62 @@ import SwiftUI
 struct Home: View {
     
     var window = NSScreen.main?.visibleFrame
+    @State private var search = ""
     
     var body: some View {
         HStack {
             
             SideBar()
             
-            Spacer()
+            VStack {
+                
+                HStack(spacing: 12) {
+                    
+                    // Search Bar
+                    
+                    HStack(spacing: 15) {
+                        
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                        
+                        TextField("Search", text: $search)
+                            .textFieldStyle(PlainTextFieldStyle())
+                    }
+                    .padding(.vertical, 10)
+                    .padding(.horizontal)
+                    .background(BlurWindow())
+                    .cornerRadius(10)
+                    
+                    Button(action: {}, label: {
+                        
+                        Image(systemName: "slider.vertical.3")
+                            .foregroundColor(.black)
+                            .padding(10)
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .shadow(color: Color.black.opacity(0.1), radius: 5, x: 5, y: 5)
+                            .shadow(color: Color.black.opacity(0.1), radius: 5, x: -5, y: -5)
+                    })
+                    .buttonStyle(PlainButtonStyle())
+                    
+                    Button(action: {}, label: {
+                        
+                        Image(systemName: "plus")
+                            .foregroundColor(.white)
+                            .padding(10)
+                            .background(Color.black)
+                            .cornerRadius(10)
+                            .shadow(color: Color.black.opacity(0.1), radius: 5, x: 5, y: 5)
+                            .shadow(color: Color.black.opacity(0.1), radius: 5, x: -5, y: -5)
+                    })
+                    .buttonStyle(PlainButtonStyle())
+                }
+                
+                // Scroll View
+                
+                Spacer()
+            }
+            .padding()
             
         }
         .ignoresSafeArea(.all, edges: .all)
@@ -34,8 +83,8 @@ struct Home_Previews: PreviewProvider {
 
 struct SideBar: View {
     
-    @State var selected = "Home"
-    @Namespace var animation
+    @State private var selected = "Home"
+    @Namespace private var animation
     
     var body: some View {
         HStack(spacing: 0) {
@@ -151,4 +200,14 @@ struct SideBar: View {
         } //: SIDE BAR
         .frame(width: 240)
     }
+}
+
+extension NSTextField {
+    
+    // REMOVE FOCUS RING
+    open override var focusRingType: NSFocusRingType {
+        get { .none }
+        set {}
+    }
+    
 }
