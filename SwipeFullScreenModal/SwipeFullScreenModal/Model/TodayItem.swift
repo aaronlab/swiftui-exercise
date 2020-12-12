a438659759c93a39ac3cf2da23cde4976a1859a0
Aaron Lee <aaronlab.net@gmail.com>
2020-12-12 16:47:45 +0900

    Update TodayItem.swift

--- SwipeFullScreenModal/SwipeFullScreenModal/Model/TodayItem.swift
+++ SwipeFullScreenModal/SwipeFullScreenModal/Model/TodayItem.swift
@@ -18,5 +18,5 @@ struct TodayItem: Identifiable {
 
 var items = [
     TodayItem(title: "Froza Street", category: "Ultimate Street Racing Game", overlay: "GAME OF THE DAY", contentImage: "b1", logo: "l1"),
-    TodayItem(title: "Roblox", category: "Adventure", overlay: "Li Nas X Performs In Roblox", contentImage: "b2", logo: "l2 ")
+    TodayItem(title: "Roblox", category: "Adventure", overlay: "Li Nas X Performs In Roblox", contentImage: "b2", logo: "l2")
 ]
