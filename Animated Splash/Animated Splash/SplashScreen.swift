46c914ab5c9a06ca841781392ccf058f711c1d7e
Aaron Lee <aaronlab.net@gmail.com>
2020-12-12 16:18:27 +0900

    Done

--- Animated Splash/Animated Splash/SplashScreen.swift
+++ Animated Splash/Animated Splash/SplashScreen.swift
@@ -10,11 +10,25 @@ import Lottie
 
 struct SplashScreen: View {
     
+    @State var show = false
+    
     var body: some View {
         
         VStack {
             
-            AnimatedView()
+            ZStack {
+                
+                Color("bg")
+                    .ignoresSafeArea()
+                
+                AnimatedView(show: $show)
+                    .frame(height: UIScreen.main.bounds.height / 2)
+                
+                if show {
+                    Color.white
+                        .ignoresSafeArea()
+                }
+            }
         }
         
     }
@@ -23,13 +37,21 @@ struct SplashScreen: View {
 
 struct AnimatedView: UIViewRepresentable {
     
+    @Binding var show: Bool
+    
     func makeUIView(context: Context) -> AnimationView {
-        let view = AnimationView(name: "splash.json", bundle: Bundle.main)
-        view.play()
+        let view = AnimationView(name: "splash", bundle: Bundle.main)
+        view.play { status in
+            if status {
+                
+                withAnimation(.interactiveSpring(response: 0.7, dampingFraction: 0.8, blendDuration: 0.8)) {
+                    self.show.toggle()
+                }
+            }
+        }
         return view
     }
     
     func updateUIView(_ uiView: AnimationView, context: Context) {
-        
     }
 }
