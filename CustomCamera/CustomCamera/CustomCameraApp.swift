0d4ce06fabc0ec9bda73b5b9ee7475e6db58f674
Aaron Lee <aaronlab.net@gmail.com>
2020-12-13 11:48:33 +0900

    Init

---
+++ CustomCamera/CustomCamera/CustomCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCameraApp.swift
+//  CustomCamera
+//
+//  Created by Aaron Lee on 2020-12-13.
+//
+
+import SwiftUI
+
+@main
+struct CustomCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
