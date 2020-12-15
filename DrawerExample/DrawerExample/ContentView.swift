227cbd9dee6b39d75e1a0bc209b1a05922b48069
Aaron Lee <aaronlab.net@gmail.com>
2020-12-15 19:26:43 +0900

    Init

---
+++ DrawerExample/DrawerExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DrawerExample
+//
+//  Created by Aaron Lee on 2020-12-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DrawerExample/DrawerExample/DrawerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawerExampleApp.swift
+//  DrawerExample
+//
+//  Created by Aaron Lee on 2020-12-15.
+//
+
+import SwiftUI
+
+@main
+struct DrawerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
