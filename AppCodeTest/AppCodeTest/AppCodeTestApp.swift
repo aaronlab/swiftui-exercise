29633fa75944d8208a1385971d9de83d58fb32f7
Aaron Lee <aaronlab.net@gmail.com>
2020-12-16 21:31:35 +0900

    Init

---
+++ AppCodeTest/AppCodeTest/AppCodeTestApp.swift
@@ -0,0 +1,18 @@
+//
+//  AppCodeTestApp.swift
+//  AppCodeTest
+//
+//  Created by Aaron Lee on 2020-12-16.
+//
+//
+
+import SwiftUI
+
+@main
+struct AppCodeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
