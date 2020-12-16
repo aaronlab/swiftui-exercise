01508af7ac0ce321e6a3c413231259929f1e3235
Aaron Lee <aaronlab.net@gmail.com>
2020-12-16 22:42:24 +0900

    .

--- HackerNews/HackerNews/ContentView.swift
+++ HackerNews/HackerNews/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            Text("Hello")
+        }
+        .navigationBarTitle("Hacker News")
     }
 }
 
