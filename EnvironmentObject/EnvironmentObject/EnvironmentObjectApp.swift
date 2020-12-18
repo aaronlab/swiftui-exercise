9fa3fc529230b89cd9ce533711926e4724db207e
Aaron Lee <aaronlab.net@gmail.com>
2020-12-18 21:47:14 +0900

    MyObject

--- EnvironmentObject/EnvironmentObject/EnvironmentObjectApp.swift
+++ EnvironmentObject/EnvironmentObject/EnvironmentObjectApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct EnvironmentObjectApp: App {
+    
+    let myObj = MyObject()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(myObj)
         }
     }
 }
---
+++ EnvironmentObject/EnvironmentObject/MyObject.swift
@@ -0,0 +1,14 @@
+//
+//  MyObject.swift
+//  EnvironmentObject
+//
+//  Created by Aaron Lee on 2020-12-18.
+//
+
+import Foundation
+
+final class MyObject: ObservableObject {
+    
+    @Published var numberOfClicks = 0
+    
+}
