a48f602e5bb665b48c199921c86c3a938a85b2b2
Aaron Lee <aaronlab.net@gmail.com>
2020-12-18 21:30:04 +0900

    Environment Object

---
+++ EnvironmentObject/EnvironmentObject/EnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjectApp.swift
+//  EnvironmentObject
+//
+//  Created by Aaron Lee on 2020-12-18.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
