e52cbabe4d0b9ee11f8a217312da6d9d1b54693c
Aaron Lee <aaronlab.net@gmail.com>
2020-12-20 18:36:18 +0900

    Create Colors.swift

---
+++ Shapes/Shapes/Colors.swift
@@ -0,0 +1,13 @@
+//
+//  Colors.swift
+//  Shapes
+//
+//  Created by Aaron Lee on 2020-12-20.
+//
+
+import SwiftUI
+
+struct Colors {
+    static let color1 = Color.pink
+    static let color2 = Color.green
+}
