c5eb06fc429eafc8339e4e01cc9f91a2ef15eea6
Aaron Lee <aaronlab.net@gmail.com>
2020-12-20 18:36:11 +0900

    Create ShapesApp.swift

---
+++ Shapes/Shapes/ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesApp.swift
+//  Shapes
+//
+//  Created by Aaron Lee on 2020-12-20.
+//
+
+import SwiftUI
+
+@main
+struct ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
