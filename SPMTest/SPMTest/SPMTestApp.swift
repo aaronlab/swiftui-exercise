78bb4c2f32acc546bb40161fc2c376278766d1e2
Aaron Lee <aaronlab.net@gmail.com>
2020-12-21 22:12:29 +0900

    Init TestSMP

---
+++ SPMTest/SPMTest/SPMTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SPMTestApp.swift
+//  SPMTest
+//
+//  Created by Aaron Lee on 2020-12-21.
+//
+
+import SwiftUI
+
+@main
+struct SPMTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
