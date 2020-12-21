40ae5854a44204f92b0f7f9f0d838e3fea222355
Aaron Lee <aaronlab.net@gmail.com>
2020-12-21 22:13:42 +0900

    Import TestView from TestSPM

--- SPMTest/SPMTest/ContentView.swift
+++ SPMTest/SPMTest/ContentView.swift
@@ -6,11 +6,11 @@
 //
 
 import SwiftUI
+import TestSPM
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TestSPM.TestView()
     }
 }
 
