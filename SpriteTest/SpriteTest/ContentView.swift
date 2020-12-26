930dff6c1b715fd3cb20b1d301b451291c7f85fe
Aaron Lee <aaronlab.net@gmail.com>
2020-12-26 22:51:12 +0900

    Sprite Kit

---
+++ SpriteTest/SpriteTest/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  SpriteTest
+//
+//  Created by Aaron Lee on 2020-12-26.
+//
+
+import SpriteKit
+import SwiftUI
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        
+        let box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+        box.position = location
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+        addChild(box)
+    }
+}
+
+struct ContentView: View {
+    
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width: 300, height: 400)
+        scene.scaleMode = .fill
+        return scene
+    }
+    
+    var body: some View {
+        SpriteView(scene: scene)
+            .frame(width: 300, height: 400)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpriteTest/SpriteTest/SpriteTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteTestApp.swift
+//  SpriteTest
+//
+//  Created by Aaron Lee on 2020-12-26.
+//
+
+import SwiftUI
+
+@main
+struct SpriteTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
