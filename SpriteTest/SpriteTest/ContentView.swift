1c65d07e4416548739d08182bdbbe6d70c42da02
Aaron Lee <aaronlab.net@gmail.com>
2020-12-26 22:54:12 +0900

    Edit ColorScheme

--- SpriteTest/SpriteTest/ContentView.swift
+++ SpriteTest/SpriteTest/ContentView.swift
@@ -37,6 +37,7 @@ struct ContentView: View {
     var body: some View {
         SpriteView(scene: scene)
             .frame(width: 300, height: 400)
+            .preferredColorScheme(.light)
     }
 }
 
