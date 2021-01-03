445bf14a9047d0ec7288c5ad7717af4cd6f2b9f9
Aaron Lee <aaronlab.net@gmail.com>
2021-01-03 17:15:04 +0900

    Init SQLiteExample

---
+++ SQLiteExample/SQLiteExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SQLiteExample
+//
+//  Created by Aaron Lee on 2021-01-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SQLiteExample/SQLiteExample/SQLiteExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SQLiteExampleApp.swift
+//  SQLiteExample
+//
+//  Created by Aaron Lee on 2021-01-03.
+//
+
+import SwiftUI
+
+@main
+struct SQLiteExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
