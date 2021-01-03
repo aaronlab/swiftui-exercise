5610d5a2c9e92c3b26b4798116854a8df38529c9
Aaron Lee <aaronlab.net@gmail.com>
2021-01-03 17:17:00 +0900

    Add Views

---
+++ SQLiteExample/SQLiteExample/Views/AddView.swift
@@ -0,0 +1,20 @@
+//
+//  AddView.swift
+//  SQLiteExample
+//
+//  Created by Aaron Lee on 2021-01-03.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView()
+    }
+}
---
+++ SQLiteExample/SQLiteExample/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  SQLiteExample
+//
+//  Created by Aaron Lee on 2021-01-03.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
