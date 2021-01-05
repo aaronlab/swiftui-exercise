d6d2f1834e9cdb0776bac3703d2c50468d63a7fe
Aaron Lee <aaronlab.net@gmail.com>
2021-01-06 01:00:59 +0900

    Init ImagePinchZoom

---
+++ ImagePinchZoom/ImagePinchZoom/ImagePinchZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImagePinchZoomApp.swift
+//  ImagePinchZoom
+//
+//  Created by Aaron Lee on 2021-01-06.
+//
+
+import SwiftUI
+
+@main
+struct ImagePinchZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
