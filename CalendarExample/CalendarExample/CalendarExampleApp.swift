cad0e51a8e6c2c23bec348f89d4d0292954eb207
Aaron Lee <aaronlab.net@gmail.com>
2021-01-06 23:19:14 +0900

    Init

---
+++ CalendarExample/CalendarExample/CalendarExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarExampleApp.swift
+//  CalendarExample
+//
+//  Created by Aaron Lee on 2021-01-06.
+//
+
+import SwiftUI
+
+@main
+struct CalendarExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
