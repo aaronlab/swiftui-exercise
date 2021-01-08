41817f2fc7044709d28af03f66e8dac020355ec3
Aaron Lee <aaronlab.net@gmail.com>
2021-01-08 22:30:00 +0900

    Init

---
+++ NewsTestApp/NewsTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsTestApp
+//
+//  Created by Aaron Lee on 2021-01-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsTestApp/NewsTestApp/NewsTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsTestAppApp.swift
+//  NewsTestApp
+//
+//  Created by Aaron Lee on 2021-01-08.
+//
+
+import SwiftUI
+
+@main
+struct NewsTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
