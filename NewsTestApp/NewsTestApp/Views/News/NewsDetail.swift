46174690922da88e15b85cb16edfcbb7a054265e
Aaron Lee <aaronlab.net@gmail.com>
2021-01-08 22:35:01 +0900

    Project Setup

---
+++ NewsTestApp/NewsTestApp/Views/News/NewsDetail.swift
@@ -0,0 +1,20 @@
+//
+//  NewsDetail.swift
+//  NewsTestApp
+//
+//  Created by Aaron Lee on 2021-01-08.
+//
+
+import SwiftUI
+
+struct NewsDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewsDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsDetail()
+    }
+}
---
+++ NewsTestApp/NewsTestApp/Views/News/NewsHome.swift
@@ -0,0 +1,20 @@
+//
+//  NewsHome.swift
+//  NewsTestApp
+//
+//  Created by Aaron Lee on 2021-01-08.
+//
+
+import SwiftUI
+
+struct NewsHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewsHome_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsHome()
+    }
+}
