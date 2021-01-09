c8cb68e236d47f7e52809ef80f42caeb154ce861
Aaron Lee <aaronlab.net@gmail.com>
2021-01-09 19:41:36 +0900

    Test

--- APITest/APITest/Core/API/APISession.swift
+++ APITest/APITest/Core/API/APISession.swift
@@ -11,7 +11,31 @@ import Combine
 struct APISession: APIService {
     func request<T>(with builder: RequestBuilder) -> AnyPublisher<T, APIError> where T : Decodable {
         
-        return URLSession
+        return URLSession.shared
+            .dataTaskPublisher(for: builder.urlRequest)
+            .receive(on: RunLoop.main)
+            .mapError { _ in APIError.unknown }
+            .flatMap { data, response -> AnyPublisher<T, APIError> in
+                if let response = response as? HTTPURLResponse {
+                    
+                    if (200...299).contains(response.statusCode) {
+                        let decoder = JSONDecoder()
+                        
+                        return Just(data)
+                            .decode(type: T.self, decoder: decoder)
+                            .mapError { _ in APIError.decodingError }
+                            .eraseToAnyPublisher()
+                    } else {
+                        
+                        return Fail(error: APIError.httpError(response.statusCode))
+                            .eraseToAnyPublisher()
+                    }
+                }
+                
+                return Fail(error: APIError.unknown)
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
         
     }
 }
