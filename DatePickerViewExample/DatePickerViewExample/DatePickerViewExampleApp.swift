d4a217186a882a40cafdae229a559ab06d403e78
Aaron Lee <aaronlab.net@gmail.com>
2021-01-10 21:24:41 +0900

    Init

---
+++ DatePickerViewExample/DatePickerViewExample/DatePickerViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerViewExampleApp.swift
+//  DatePickerViewExample
+//
+//  Created by Aaron Lee on 2021/01/10.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
