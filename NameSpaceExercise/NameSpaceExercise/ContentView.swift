a783f93c1bd37ec70b56680239c2303d047dce37
Aaron Lee <aaronlab.net@gmail.com>
2021-01-11 21:32:20 +0900

    NameSpace ContentView

--- NameSpaceExercise/NameSpaceExercise/ContentView.swift
+++ NameSpaceExercise/NameSpaceExercise/ContentView.swift
@@ -8,9 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var rect: Rect = .first
+    @State private var zoomRect: Bool = false
+    @Namespace var animation
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            
+            HStack {
+                if !self.zoomRect {
+                    
+                    Rectangle()
+                        .matchedGeometryEffect(id: Rect.first, in: self.animation)
+                        .frame(width: 100, height: 100)
+                        .onTapGesture {
+                            withAnimation {
+                                self.rect = .first
+                                self.zoomRect.toggle()
+                            }
+                        }
+                    
+                    Rectangle()
+                        .matchedGeometryEffect(id: Rect.second, in: self.animation)
+                        .frame(width: 100, height: 100)
+                        .onTapGesture {
+                            withAnimation {
+                                self.rect = .second
+                                self.zoomRect.toggle()
+                            }
+                        }
+                    
+                    Rectangle()
+                        .matchedGeometryEffect(id: Rect.third, in: self.animation)
+                        .frame(width: 100, height: 100)
+                        .onTapGesture {
+                            withAnimation {
+                                self.rect = .third
+                                self.zoomRect.toggle()
+                            }
+                        }
+                }
+                
+            } //: H
+            
+            if self.zoomRect {
+                
+                ZoomedView(rect: self.rect, zoomRect: self.$zoomRect, animation: self.animation)
+            }
+            
+        }
     }
 }
 
