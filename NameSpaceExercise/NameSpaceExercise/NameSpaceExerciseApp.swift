06d6379f2811ebea90c29989136288860e7bd965
Aaron Lee <aaronlab.net@gmail.com>
2021-01-11 21:15:56 +0900

    Init

---
+++ NameSpaceExercise/NameSpaceExercise/NameSpaceExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameSpaceExerciseApp.swift
+//  NameSpaceExercise
+//
+//  Created by Aaron Lee on 2021/01/11.
+//
+
+import SwiftUI
+
+@main
+struct NameSpaceExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
