3877cb11fb59c1d54adc3ed41ae0fc6f5e6aa1bd
Aaron Lee <aaronlab.net@gmail.com>
2021-01-13 22:34:21 +0900

    User Info

--- CustomPropertyWrapper/CustomPropertyWrapper/UserInfo.swift
+++ CustomPropertyWrapper/CustomPropertyWrapper/UserInfo.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+@propertyWrapper
+struct UserInfo {
+    private let key: String
+    
+    var wrappedValue: String {
+        get { UserDefaults.standard.string(forKey: key) ?? "" }
+        set { UserDefaults.standard.set(newValue, forKey: key) }
+    }
+    
+    init(key: String) {
+        self.key = key
+    }
+}
