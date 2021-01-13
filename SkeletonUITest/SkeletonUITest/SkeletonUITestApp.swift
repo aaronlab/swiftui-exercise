ffe6a283a6dc0eeb15e3c25aa1ef230b0dd5c9c3
Aaron Lee <aaronlab.net@gmail.com>
2021-01-13 23:10:36 +0900

    Init SkeletonUITestApp

---
+++ SkeletonUITest/SkeletonUITest/SkeletonUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkeletonUITestApp.swift
+//  SkeletonUITest
+//
+//  Created by Aaron Lee on 2021/01/13.
+//
+
+import SwiftUI
+
+@main
+struct SkeletonUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
