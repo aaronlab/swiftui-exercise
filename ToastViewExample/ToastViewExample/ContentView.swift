d4e9f413bdfb287107e4ee2856d6bbcb582f81c2
Aaron Lee <aaronlab.net@gmail.com>
2021-01-14 01:28:29 +0900

    Toast View Init

---
+++ ToastViewExample/ToastViewExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToastViewExample
+//
+//  Created by Aaron Lee on 2021/01/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToastViewExample/ToastViewExample/ToastViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastViewExampleApp.swift
+//  ToastViewExample
+//
+//  Created by Aaron Lee on 2021/01/14.
+//
+
+import SwiftUI
+
+@main
+struct ToastViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
