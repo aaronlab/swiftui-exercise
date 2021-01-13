48ef0e221052a4a6e792410bfed65f73524dc21b
Aaron Lee <aaronlab.net@gmail.com>
2021-01-13 23:18:41 +0900

    Skeleton

--- SkeletonUITest/SkeletonUITest/ContentView.swift
+++ SkeletonUITest/SkeletonUITest/ContentView.swift
@@ -6,11 +6,47 @@
 //
 
 import SwiftUI
+import SkeletonUI
+
+struct User: Identifiable {
+    let id = UUID()
+    let name: String
+}
 
 struct ContentView: View {
+    
+    @State private var users = [String]()
+    @State private var users2 = [User]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            SkeletonList(with: users2, quantity: 100) { (loading, user) in
+                
+                Text(user?.name)
+                    .skeleton(with: loading)
+                    .shape(type: .rectangle)
+                    .appearance(type: .solid(color: .red, background: .blue))
+                    .multiline(lines: 3, scales: [1: 0.5])
+                    .animation(type: .pulse())
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                    self.users2 = [User(name: "John Doe"),
+                                  User(name: "Jane Doe"),
+                                  User(name: "James Doe"),
+                                  User(name: "Judy Doe")]
+                }
+            }
+            
+//            Text("Finished \(self.users.count)")
+//                .skeleton(with: users.isEmpty)
+//                .onAppear {
+//                    DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+//                        self.users = ["John Doe", "Jane Doe", "James Doe", "Judy Doe"]
+//                    }
+//                }
+        }
     }
 }
 
