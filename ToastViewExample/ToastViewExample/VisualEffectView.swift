ed56fe385c933f3484a20661c147218257f432f9
Aaron Lee <aaronlab.net@gmail.com>
2021-01-14 01:46:45 +0900

    VisualEffectView

---
+++ ToastViewExample/ToastViewExample/VisualEffectView.swift
@@ -0,0 +1,18 @@
+//
+//  VisualEffectView.swift
+//  ToastViewExample
+//
+//  Created by Aaron Lee on 2021/01/14.
+//
+
+import SwiftUI
+
+struct VisualEffectView: UIViewRepresentable {
+    var effect: UIVisualEffect?
+    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView {
+        UIVisualEffectView()
+    }
+    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) {
+        uiView.effect = self.effect
+    }
+}
