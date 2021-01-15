acd391b86e6aa2e90cca6bac6a8fc64462197ed7
Aaron Lee <aaronlab.net@gmail.com>
2021-01-15 19:32:12 +0900

    Init Tab Pages

---
+++ TabPages/TabPages/TabPagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabPagesApp.swift
+//  TabPages
+//
+//  Created by Aaron Lee on 2021/01/15.
+//
+
+import SwiftUI
+
+@main
+struct TabPagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
