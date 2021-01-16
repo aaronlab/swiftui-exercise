8617216d01877cd706cee8149f1f5a73bdca2d78
Aaron Lee <aaronlab.net@gmail.com>
2021-01-16 12:12:34 +0900

    Init Alert TextField

---
+++ AlertTextField/AlertTextField/AlertTextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertTextFieldApp.swift
+//  AlertTextField
+//
+//  Created by Aaron Lee on 2021/01/16.
+//
+
+import SwiftUI
+
+@main
+struct AlertTextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
