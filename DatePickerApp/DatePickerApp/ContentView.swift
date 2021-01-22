6920b51c0546051e1cb24d8653a93ad276de6c8b
Aaron Lee <aaronlab.net@gmail.com>
2021-01-22 18:06:11 +0900

    Init Date Picker

---
+++ DatePickerApp/DatePickerApp/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  DatePickerApp
+//
+//  Created by Aaron Lee on 2021/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct Calendar: View {
+    
+    var body: some View {
+        Text("")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DatePickerApp/DatePickerApp/DatePickerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePickerAppApp.swift
+//  DatePickerApp
+//
+//  Created by Aaron Lee on 2021/01/21.
+//
+
+import SwiftUI
+
+@main
+struct DatePickerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
