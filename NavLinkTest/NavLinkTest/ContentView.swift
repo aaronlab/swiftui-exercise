cbde917c3ad182aca96f7ec7cdbd047c6d247655
Aaron Lee <aaronlab.net@gmail.com>
2021-02-04 22:24:15 +0900

    init navlink

---
+++ NavLinkTest/NavLinkTest/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NavLinkTest
+//
+//  Created by Aaron Lee on 2021/02/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            
+            Text("Home")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavLinkTest/NavLinkTest/NavLinkTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavLinkTestApp.swift
+//  NavLinkTest
+//
+//  Created by Aaron Lee on 2021/02/04.
+//
+
+import SwiftUI
+
+@main
+struct NavLinkTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
