ba081efb2a7ef1f9d480937b1760322a9ac75f95
Aaron Lee <aaronlab.net@gmail.com>
2021-02-04 22:30:41 +0900

    content view

--- NavLinkTest/NavLinkTest/ContentView.swift
+++ NavLinkTest/NavLinkTest/ContentView.swift
@@ -11,13 +11,43 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             
-            Text("Home")
+            VStack {
+                NavigationLink("Detail", destination: DetailView())
+                    .isDetailLink(false)
+                
+                Text("Home")
+            }
+            .navigationBarTitle("Home", displayMode: .inline)
         }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct DetailView: View {
+    
+    @Environment(\.presentationMode) private var presentationMode
+    
+    var body: some View {
+        
+        Text("Detail")
+            .navigationBarBackButtonHidden(true)
+            .navigationBarTitle("Detail", displayMode: .inline)
+            .navigationBarItems(leading: Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }, label: {
+                Text("Back")
+            }))
+        
+    }
+    
+}
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
     }
 }
