c8e0b7504df5c9618455aa6038bfd50a9710d0d6
Aaron Lee <aaronlab.net@gmail.com>
2021-02-10 23:13:24 +0900

    feat: init view builder

---
+++ ViewBuilderTest/ViewBuilderTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewBuilderTest
+//
+//  Created by Aaron Lee on 2021/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ViewBuilderTest/ViewBuilderTest/ViewBuilderTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewBuilderTestApp.swift
+//  ViewBuilderTest
+//
+//  Created by Aaron Lee on 2021/02/10.
+//
+
+import SwiftUI
+
+@main
+struct ViewBuilderTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
