4909d719acca4ae5cbb36b4ddb8e5fedbe4cf48a
Aaron Lee <aaronlab.net@gmail.com>
2021-02-23 21:01:11 +0900

    init proj

---
+++ SpriteKitTest/SpriteKitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpriteKitTest
+//
+//  Created by Aaron Lee on 2021/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpriteKitTest/SpriteKitTest/SpriteKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitTestApp.swift
+//  SpriteKitTest
+//
+//  Created by Aaron Lee on 2021/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
