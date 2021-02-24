00ab6f2c84dc56bed6123198e05fdece7d9c4cb5
Aaron Lee <aaronlab.net@gmail.com>
2021-02-24 22:38:56 +0900

    init proj

---
+++ WeatherTest/WeatherTest/WeatherTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTestApp.swift
+//  WeatherTest
+//
+//  Created by Aaron Lee on 2021/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
